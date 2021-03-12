c16274bb1bebc46be0c2e61b0ab340a3dd08993b
paniczklos <panicz.klos@gmail.com>
2021-03-12 12:39:48 +0100

    Update server.js

--- express/server.js
+++ express/server.js
@@ -49,9 +49,9 @@ function generateStringSVGFromHash(hash) {
 
 function generateMetadata(req, res) {
   const _id = req.params.id;
-  if req.params.id.includes(".") (
+  if (req.params.id.includes("svg")) {
    _id = (req.params.id.split('.').slice(0, -1)).join('.');
-  )
+  }
   const hash = ethers.BigNumber.from(_id).toHexString();
   const truncated = hash.slice(0, 20); // 0x + 9 bytes
   const svg = generateStringSVGFromHash(hash);
